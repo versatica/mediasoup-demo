aab35cc7f7c8fa8ac1b57380e76fb268c5bff5b6
Iñaki Baz Castillo <ibc@aliax.net>
2019-03-22 18:12:01 +0100

    config.example.js: Add two H264 codec configurations

--- server/config.example.js
+++ server/config.example.js
@@ -68,7 +68,19 @@ module.exports =
 					clockRate  : 90000,
 					parameters :
 					{
-						'x-google-start-bitrate': 1500
+						'x-google-start-bitrate' : 1000
+					}
+				},
+				{
+					kind       : 'video',
+					mimeType   : 'video/h264',
+					clockRate  : 90000,
+					parameters :
+					{
+						'packetization-mode'      : 1,
+						'profile-level-id'        : '4d0032',
+						'level-asymmetry-allowed' : 1,
+						'x-google-start-bitrate'  : 1000
 					}
 				},
 				{
@@ -79,7 +91,8 @@ module.exports =
 					{
 						'packetization-mode'      : 1,
 						'profile-level-id'        : '42e01f',
-						'level-asymmetry-allowed' : 1
+						'level-asymmetry-allowed' : 1,
+						'x-google-start-bitrate'  : 1000
 					}
 				}
 			]
