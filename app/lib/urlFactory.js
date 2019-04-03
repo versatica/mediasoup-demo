34bcb2e800dca31d82fbbd9fceeb22aa3be51187
Iñaki Baz Castillo <ibc@aliax.net>
2019-04-03 18:02:07 +0200

    add stuff for test

--- app/lib/urlFactory.js
+++ app/lib/urlFactory.js
@@ -1,9 +1,12 @@
-const PROTOO_PORT = 4443;
+let protooPort = 4443;
+
+if (window.location.hostname === 'test.mediasoup.org')
+	protooPort = 4444;
 
 export function getProtooUrl({ roomId, peerId, forceH264 })
 {
 	const hostname = window.location.hostname;
-	let url = `wss://${hostname}:${PROTOO_PORT}/?roomId=${roomId}&peerId=${peerId}`;
+	let url = `wss://${hostname}:${protooPort}/?roomId=${roomId}&peerId=${peerId}`;
 
 	if (forceH264)
 		url = `${url}&forceH264=true`;
