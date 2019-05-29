a67cc23ac8c128286f0c8a76046470977651b4fb
Iñaki Baz Castillo <ibc@aliax.net>
2019-05-29 14:04:37 +0200

    Document much more within config.example.js

--- server/config.example.js
+++ server/config.example.js
@@ -1,35 +1,39 @@
 /**
- * IMPORTANT:
+ * IMPORTANT (PLEASE READ THIS):
  *
  * This is not the "configuration file" of mediasoup. This is the configuration
- * file of the mediasoup-demo app. mediasoup itself is a server-side library, It
+ * file of the mediasoup-demo app. mediasoup itself is a server-side library, it
  * does not read any "configuration file". Instead it exposes an API. This demo
- * application just reads settings from this file and calls the mediasoup API with
- * them when appropriate.
+ * application just reads settings from this file (once copied to config.js) and
+ * calls the mediasoup API with those settings when appropriate.
  */
 
 const os = require('os');
 
 module.exports =
 {
-	// Listening hostname (just for for `gulp live|open` tasks).
+	// Listening hostname (just for `gulp live` or `gulp open` tasks).
 	domain : 'localhost',
-	// Signaling settings.
-	https :
+	// Signaling settings (protoo WebSocket server and HTTP API server).
+	https  :
 	{
 		listenIp   : '0.0.0.0',
-		listenPort : 4443, // NOTE: Don't change it (client app assumes 4443).
+		// NOTE: Don't change listenPort (client app assumes 4443).
+		listenPort : 4443,
+		// NOTE: Set your own valid certificate files.
 		tls        :
 		{
 			cert : `${__dirname}/certs/mediasoup-demo.localhost.cert.pem`,
 			key  : `${__dirname}/certs/mediasoup-demo.localhost.key.pem`
 		}
 	},
-	// Media settings.
+	// mediasoup settings.
 	mediasoup :
 	{
+		// Number of mediasoup workers to launch.
 		numWorkers : Object.keys(os.cpus()).length,
 		// mediasoup Worker settings.
+		// See https://mediasoup.org/documentation/v3/mediasoup/api/#WorkerSettings
 		worker     :
 		{
 			logLevel : 'warn',
@@ -50,10 +54,10 @@ module.exports =
 			rtcMinPort : 40000,
 			rtcMaxPort : 49999
 		},
-		// mediasoup Router settings.
+		// mediasoup Router options.
+		// See https://mediasoup.org/documentation/v3/mediasoup/api/#RouterOptions
 		router :
 		{
-			// Router media codecs.
 			mediaCodecs :
 			[
 				{
@@ -97,7 +101,8 @@ module.exports =
 				}
 			]
 		},
-		// mediasoup WebRtcTransport settings.
+		// mediasoup WebRtcTransport options.
+		// See https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcTransportOptions
 		webRtcTransport :
 		{
 			listenIps :
