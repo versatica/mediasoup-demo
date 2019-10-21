1181529ba66f591cda508d7d02ec8ea584b69a0e
haiyangwu <wuhaiyang1213@gmail.com>
2019-10-21 20:03:34 +0800

    win support (#56)

--- server/lib/interactiveClient.js
+++ server/lib/interactiveClient.js
@@ -1,6 +1,10 @@
 const net = require('net');
+const os = require('os');
+const path = require('path');
 
-const SOCKET_PATH = '/tmp/mediasoup-demo.sock';
+const SOCKET_PATH_UNIX = '/tmp/mediasoup-demo.sock';
+const SOCKET_PATH_WIN = path.join('\\\\?\\pipe', process.cwd(), 'mediasoup-demo');
+const SOCKET_PATH = os.platform() === 'win32'? SOCKET_PATH_WIN : SOCKET_PATH_UNIX
 
 module.exports = async function()
 {
