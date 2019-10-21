181419f20eaec2eec0c7c65e8874318a77ab59e8
Iñaki Baz Castillo <ibc@aliax.net>
2019-10-21 12:55:12 +0200

    Fix typo and make MEDIASOUP_LISTEN_IP (for Docker) be "0.0.0.0" by default

--- server/config.example.js
+++ server/config.example.js
@@ -23,8 +23,8 @@ module.exports =
 		// NOTE: Set your own valid certificate files.
 		tls        :
 		{
-			cert : process.env.HTTPS_CERT_FULLCHAIN `${__dirname}/certs/fullchain.pem`,
-			key  : process.env.HTTPS_CERT_PRIVKEY `${__dirname}/certs/privkey.pem`
+			cert : process.env.HTTPS_CERT_FULLCHAIN || `${__dirname}/certs/fullchain.pem`,
+			key  : process.env.HTTPS_CERT_PRIVKEY || `${__dirname}/certs/privkey.pem`
 		}
 	},
 	// mediasoup settings.
