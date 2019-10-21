dcb5c67d8fddfbdafd87e06c889505ee8f5dcc0e
Iñaki Baz Castillo <ibc@aliax.net>
2019-10-21 14:04:47 +0200

    Add missing colon

--- server/lib/interactiveClient.js
+++ server/lib/interactiveClient.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const SOCKET_PATH_UNIX = '/tmp/mediasoup-demo.sock';
 const SOCKET_PATH_WIN = path.join('\\\\?\\pipe', process.cwd(), 'mediasoup-demo');
-const SOCKET_PATH = os.platform() === 'win32'? SOCKET_PATH_WIN : SOCKET_PATH_UNIX
+const SOCKET_PATH = os.platform() === 'win32'? SOCKET_PATH_WIN : SOCKET_PATH_UNIX;
 
 module.exports = async function()
 {
