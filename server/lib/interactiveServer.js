bcd5a9d1ed7d29cef2f8de5bbeae4e7ba06b19e4
HaiYang Wu <wuhaiyang1213@gmail.com>
2019-10-21 16:51:42 +0800

    format and rename SOCKET_PATH_UIX to SOCKET_PATH_UNIX

--- server/lib/interactiveServer.js
+++ server/lib/interactiveServer.js
@@ -8,9 +8,9 @@ const mediasoup = require('mediasoup');
 const colors = require('colors/safe');
 const pidusage = require('pidusage');
 
-const SOCKET_PATH_UIX = '/tmp/mediasoup-demo.sock';
+const SOCKET_PATH_UNIX = '/tmp/mediasoup-demo.sock';
 const SOCKET_PATH_WIN = path.join('\\\\?\\pipe', process.cwd(), 'mediasoup-demo');
-const SOCKET_PATH = os.platform() === 'win32' ? SOCKET_PATH_WIN : SOCKET_PATH_UIX;
+const SOCKET_PATH = os.platform() === 'win32' ? SOCKET_PATH_WIN : SOCKET_PATH_UNIX;
 
 // Maps to store all mediasoup objects.
 const workers = new Map();
