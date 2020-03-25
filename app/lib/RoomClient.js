032019990ebd17cb167b7973e786766878853221
Iñaki Baz Castillo <ibc@aliax.net>
2020-03-25 22:32:32 +0100

    cosmetic

--- app/lib/RoomClient.js
+++ app/lib/RoomClient.js
@@ -794,6 +794,9 @@ export default class RoomClient
 						opusStereo : 1,
 						opusDtx    : 1
 					}
+					// NOTE: for testing codec selection.
+					// codec : this._mediasoupDevice.rtpCapabilities.codecs
+					// 	.find((codec) => codec.mimeType.toLowerCase() === 'audio/pcma')
 				});
 
 			store.dispatch(stateActions.addProducer(
@@ -997,6 +1000,9 @@ export default class RoomClient
 						{
 							videoGoogleStartBitrate : 1000
 						}
+						// NOTE: for testing codec selection.
+						// codec : this._mediasoupDevice.rtpCapabilities.codecs
+						// 	.find((codec) => codec.mimeType.toLowerCase() === 'video/h264')
 					});
 			}
 			else
