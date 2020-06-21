3ea970e59f499ae0304936862aca37a6bcce7239
Tudor <tudor@swisstch.com>
2020-06-21 18:55:31 +0200

    undo audi track request on produce

--- app/lib/RoomClient.js
+++ app/lib/RoomClient.js
@@ -2135,22 +2135,22 @@ export default class RoomClient
 
 			await this._mediasoupDevice.load({ routerRtpCapabilities });
 
+			// NOTE: Stuff to play remote audios due to browsers' new autoplay policy.
+			//
+			// Just get access to the mic and DO NOT close the mic track for a while.
+			// Super hack!
+			{
+				const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+				const audioTrack = stream.getAudioTracks()[0];
+
+				audioTrack.enabled = false;
+
+				setTimeout(() => audioTrack.stop(), 120000);
+			}
+			
 			// Create mediasoup Transport for sending (unless we don't want to produce).
 			if (this._produce)
 			{
-				// NOTE: Stuff to play remote audios due to browsers' new autoplay policy.
-				//
-				// Just get access to the mic and DO NOT close the mic track for a while.
-				// Super hack!
-				{
-					const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-					const audioTrack = stream.getAudioTracks()[0];
-
-					audioTrack.enabled = false;
-
-					setTimeout(() => audioTrack.stop(), 120000);
-				}
-				
 				const transportInfo = await this._protoo.request(
 					'createWebRtcTransport',
 					{
