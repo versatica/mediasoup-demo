c1434fc86c371cb763f07abf0e6d6d9f3061c741
Iñaki Baz Castillo <ibc@aliax.net>
2023-01-14 12:41:25 +0100

    Add server/run.sh (which detects proper local IP) and make run-with-valgrind.sh also use it
    Usage in Mac:
    
    ```
    $ ./run.sh
    [run.sh] [INFO] detected OS: Mac
    [run.sh] [INFO] detected local IP: 192.168.1.101
    [run.sh] [INFO] running mediasoup-demo server.js with envs:
    [run.sh] [INFO] - DEBUG=*worker* *WARN* *ERROR*
    [run.sh] [INFO] - INTERACTIVE=true
    [run.sh] [INFO] - MEDIASOUP_ANNOUNCED_IP=192.168.1.101
    
    $ ./run-with-valgrind.sh
    [run.sh] [INFO] detected OS: Mac
    [run.sh] [INFO] detected local IP: 192.168.1.101
    [run.sh] [INFO] running mediasoup-demo server.js with envs:
    [run.sh] [INFO] - DEBUG=*worker* *WARN* *ERROR*
    [run.sh] [INFO] - INTERACTIVE=true
    [run.sh] [INFO] - MEDIASOUP_VALGRIND_OPTIONS=--leak-check=full
    [run.sh] [INFO] - MEDIASOUP_USE_VALGRIND=true
    [run.sh] [INFO] - MEDIASOUP_ANNOUNCED_IP=192.168.1.101
    ```
    
    Usage in Linux:
    
    ```
    $ ./run.sh
    [run.sh] [INFO] detected OS: Linux
    [run.sh] [INFO] detected local IP: 62.210.59.78
    [run.sh] [INFO] running mediasoup-demo server.js with envs:
    [run.sh] [INFO] - DEBUG=*worker* *WARN* *ERROR*
    [run.sh] [INFO] - INTERACTIVE=true
    [run.sh] [INFO] - MEDIASOUP_ANNOUNCED_IP=62.210.59.78
    
    $ ./run-with-valgrind.sh
    [run.sh] [INFO] detected OS: Linux
    [run.sh] [INFO] detected local IP: 62.210.59.78
    [run.sh] [INFO] running mediasoup-demo server.js with envs:
    [run.sh] [INFO] - DEBUG=*worker* *WARN* *ERROR*
    [run.sh] [INFO] - INTERACTIVE=true
    [run.sh] [INFO] - MEDIASOUP_ANNOUNCED_IP=62.210.59.78
    [run.sh] [INFO] - MEDIASOUP_VALGRIND_OPTIONS=--leak-check=full
    [run.sh] [INFO] - MEDIASOUP_USE_VALGRIND=true
    ```

--- server/run-with-valgrind.sh
+++ server/run-with-valgrind.sh
@@ -5,7 +5,7 @@ set -e
 export MEDIASOUP_USE_VALGRIND="true"
 export MEDIASOUP_VALGRIND_OPTIONS=${MEDIASOUP_VALGRIND_OPTIONS:="--leak-check=full --track-fds=yes --log-file=./mediasoup_valgrind_%p.log"}
 # export MEDIASOUP_VALGRIND_OPTIONS=${MEDIASOUP_VALGRIND_OPTIONS:="--leak-check=full --track-fds=yes"}
-export DEBUG="*worker* *WARN* *ERROR* mediasoup_worker*"
-export INTERACTIVE="true"
+# export DEBUG="*worker* *WARN* *ERROR*"
+# export INTERACTIVE="true"
 
-./server.js
+./run.sh
