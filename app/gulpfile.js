a1a7a810b5f81fb4016849c2e5215e25dc47fb80
Iñaki Baz Castillo <ibc@aliax.net>
2023-07-05 12:08:45 +0200

    revove gulp warning

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -55,7 +55,7 @@ const cssBase64 = require('gulp-css-base64');
 const nib = require('nib');
 const browserSync = require('browser-sync');
 
-const PKG = require('./package.json');
+const PKG = require('./package');
 const BANNER = fs.readFileSync('banner.txt').toString();
 const BANNER_OPTIONS =
 {
