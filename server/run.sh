20669662da6f9851938d30e4534166d459c343ba
Iñaki Baz Castillo <ibc@aliax.net>
2024-06-25 11:46:38 +0200

    fix colors in run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 function log_info() {
-	echo -e "\e[0;36m[run.sh] [INFO] $@\e[0m"
+	echo -e "\033[0;36m[run.sh] [INFO] $@\033[0m"
 }
 
 function log_error() {
-	echo -e "\e[1;31m[run.sh] [ERROR] $@\e[0m" 1>&2;
+	echo -e "\033[0;31m[run.sh] [INFO] $@\033[0m" 1>&2;
 }
 
 function check_os() {
