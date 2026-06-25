571c99743710c994b00fb757ce2b445bead0dcbc
Iñaki Baz Castillo <ibc@aliax.net>
2026-06-25 13:26:31 +0200

    remove app/update-mediasoup-client.sh

--- app/update-mediasoup-client.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#
-# This script updates mediasoup-client in package-lock.json to its latest commit
-# in v3 branch.
-#
-
-npm install github:versatica/mediasoup-client#v3 --legacy-peer-deps
